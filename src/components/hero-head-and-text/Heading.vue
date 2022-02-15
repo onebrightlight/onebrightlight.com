44c8abf8279288eba4df03f23a9c8caae5e2548f
Christopher Kennedy <chris@onebrightlight.com>
2022-02-14 17:22:31 -0800

    Rename Hero Wrapper, move heading to component

---
+++ src/components/hero-head-and-text/Heading.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2 class="text-3xl font-bold sm:text-4xl lg:text-5xl font-heading xl:text-6xl 2xl:text-7xl">
+    <slot />
+  </h2>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <script setup>
   import Link from '@/components/Link.vue'
-  import HeroHeadAndTextVue from '@/components/hero-head-and-text/Wrapper.vue'
+  import HeroWrapper from '@/components/hero-head-and-text/Wrapper.vue'
   import HeroHead from '@/components/hero-head-and-text/Head.vue'
+  import HeroHeading from '@/components/hero-head-and-text/Heading.vue'
   import HeroText from '@/components/hero-head-and-text/Text.vue'
   import Form from '@/components/Form.vue'
 </script>
@@ -14,35 +15,39 @@
     </div>
     <img src="/img/portrait-illustration.png" alt="Portrait illustration of me, Christopher Kennedy" class="absolute bottom-0 z-0 h-full max-w-none -right-24 sm:right-0 opacity-60 sm:opacity-80 md:opacity-100">
   </div>
-  <HeroHeadAndTextVue background="bg-jet-100 bg-light-jet-pattern">
+  <HeroWrapper background="bg-jet-100 bg-light-jet-pattern">
     <HeroHead>
-       <h2 class="text-3xl font-bold sm:text-4xl lg:text-5xl font-heading xl:text-6xl 2xl:text-7xl">I make <span class="text-tart-500">great websites</span><br class="md:hidden"> with <span class="text-tart-500">amazing people</span>.</h2>
+      <HeroHeading>
+        I make <span class="text-tart-500">great websites</span><br class="md:hidden"> with <span class="text-tart-500">amazing people</span>.
+      </HeroHeading>
     </HeroHead>
     <HeroText>
       <p class="whitespace-pre-wrap ">I’ve worked on projects rang­ing from small bou­tique web­sites to ecom­merce solu­tions for nation­al food brands, to build­ing a webapp for the <Link webUrl="https://access.nyc.gov" text="City of New York" /> while working with <Link webUrl="https://www.bluestate.co" text="Blue State Digital" />, to most recently stewarding and maintaining the <Link webUrl="https://www.oaklandca.gov" text="City of Oakland’s Website" />.</p>
       
       <p class="whitespace-pre-wrap ">In every instance, I’ve enjoyed focusing on infor­ma­tion archi­tec­ture as well as the min­u­tae of front-end devel­op­ment. If you’d like to see some of my work, <Link webUrl="#contact" text="contact me" />.</p>
     </HeroText>
-  </HeroHeadAndTextVue>
-  <HeroHeadAndTextVue background="bg-jet-700 bg-dark-jet-pattern" textColor="text-mint-600">
+  </HeroWrapper>
+  <HeroWrapper background="bg-jet-700 bg-dark-jet-pattern" textColor="text-mint-600">
     <HeroHead>
-      <h2 class="text-3xl font-bold sm:text-4xl lg:text-5xl font-heading xl:text-6xl 2xl:text-7xl">I’m online a lot.<br> Like <span class="text-tart-500">a lot</span> a lot.</h2>
+      <HeroHeading>
+        I’m online a lot.<br> Like <span class="text-tart-500">a lot</span> a lot.
+      </HeroHeading>
     </HeroHead>
     <HeroText>
       <p>I love how the internet allows us to be closer to one another, especially given the current <Link webUrl="https://www.cdc.gov/coronavirus/2019-ncov/index.html" text="health" /> and <Link webUrl="https://climate.nasa.gov" text="ecological" /> climate. We have the opportunity to expose ourselves to so many different ideas and concepts; <Link webUrl="https://www.bbvaopenmind.com/en/articles/internet-changed-everyday-life/" text="we can learn so much" /> from one another. In spite of all that, I still sometimes use TikTok.</p>
 
       <p>You can find me on <Link webUrl="https://www.twitter.com/furioursus" text="Twitter" />, <Link webUrl="https://www.github.com/onebrightlight" text="GitHub" />, <Link webUrl="https://medium.com/@onebrightlight" text="Medium" />, and <Link webUrl="https://flickr.com/photos/onebrightlight" text="Flickr" />.</p>
     </HeroText>
-  </HeroHeadAndTextVue>
-  <HeroHeadAndTextVue background="bg-jet-900 bg-darker-jet-pattern" textColor="text-mint-600" id="contact">
+  </HeroWrapper>
+  <HeroWrapper background="bg-jet-900 bg-darker-jet-pattern" textColor="text-mint-600" id="contact">
     <HeroHead>
       <div>
-        <h2 class="text-3xl font-bold sm:text-4xl lg:text-5xl font-heading xl:text-6xl 2xl:text-7xl">Get in touch!</h2>
+        <HeroHeading>Get in touch!</HeroHeading>
         <p class="mt-1 lg:max-w-lg md:mt-2 lg:text-lg xl:text-xl">If you’re interested in discussing the projects I’ve launched or you want to let me know about a project you think I’d be a match for, let’s talk!</p>
       </div>
     </HeroHead>
     <HeroText>
       <Form />
     </HeroText>
-  </HeroHeadAndTextVue>
+  </HeroWrapper>
 </template>
